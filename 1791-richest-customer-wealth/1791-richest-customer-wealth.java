94192a16e4eb1578b616ab113404174884e6981b
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-08-27 06:09:26 +0530

    Time: 0 ms (100%), Space: 42.4 MB (41.5%) - LeetHub

--- 1791-richest-customer-wealth/1791-richest-customer-wealth.java
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.java
@@ -1,23 +1,31 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        int max = 0,sum = 0;
-        int x = accounts.length;
-        if(x == 1) {
-            for(int i = 0; i < accounts[0].length;i++) {
-                max += accounts[0][i];
-               
+        // int max = 0,sum = 0;
+        // int x = accounts.length;
+        // if(x == 1) {
+        //     for(int i = 0; i < accounts[0].length;i++) {
+        //         max += accounts[0][i];
+        //     }
+        // }
+        // else {
+        // for(int i = 0; i < x; i++ ) {
+        //     if(sum > max) {
+        //         max = sum;
+        //     }
+        //     sum = 0;
+        //     for(int j = 0; j < accounts[0].length; j++) {
+        //         sum += accounts[i][j];
+        //     }
+        // }
+        // }
+        // return max;
+        var max = 0;
+        for(var customer : accounts) {
+            var sum = 0;
+            for(var bank : customer) {
+                sum += bank;
             }
-        }
-        else {
-        for(int i = 0; i < x; i++ ) {
-            if(sum > max) {
-                max = sum;
-            }
-            sum = 0;
-            for(int j = 0; j < accounts[0].length; j++) {
-                sum += accounts[i][j];
-            }
-        }
+        max = Math.max(max,sum);
         }
         return max;
     }
