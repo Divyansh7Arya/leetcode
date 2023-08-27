340ba852b3ee06312d8ec3af90b04dc87d1b21a8
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-08-27 20:45:06 +0530

    Time: 0 ms (100%), Space: 40.3 MB (99.95%) - LeetHub

--- 1505-create-target-array-in-the-given-order/1505-create-target-array-in-the-given-order.java
+++ 1505-create-target-array-in-the-given-order/1505-create-target-array-in-the-given-order.java
@@ -1,23 +1,34 @@
 class Solution {
     public int[] createTargetArray(int[] nums, int[] index) {
-        int target [] = new int [nums.length];
-        int i , j ;
-        for (var x : target) {
-            x = -1;
+
+        // int target [] = new int [nums.length];
+        // int i , j ;
+        // for (var x : target) {
+        //     x = -1;
+        // }
+        // for(i = 0; i < nums.length;i++) {
+        //     if(target[index[i]] == -1) {
+        //         target[index[i]] = nums[i];
+        //     }
+        //     else {
+        //         //shifting array 
+        //         for(j = nums.length - 1;j > index[i];j--) {
+        //             target[j] = target[j-1] ;
+        //         }
+        //         // putting value at correct position
+        //         target[index[i]] = nums[i];
+        //     }
+        // }
+        // return target;
+        int [] arr = new int[nums.length];
+        ArrayList<Integer>target=new ArrayList<Integer>();
+        for (int i = 0; i < nums.length;i++) {
+            target.add(index[i],nums[i]);
         }
-        for(i = 0; i < nums.length;i++) {
-            if(target[index[i]] == -1) {
-                target[index[i]] = nums[i];
-            }
-            else {
-                //shifting array 
-                for(j = nums.length - 1;j > index[i];j--) {
-                    target[j] = target[j-1] ;
-                }
-                // putting value at correct position
-                target[index[i]] = nums[i];
-            }
+        for (int i = 0; i < target.size();i++) {
+            arr[i] = target.get(i);
         }
-        return target;
+        return arr;
         }
     }
+
