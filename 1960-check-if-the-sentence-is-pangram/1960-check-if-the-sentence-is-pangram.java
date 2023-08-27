740fa30dfc1caf93e46d973af8506762a47a0e4d
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-08-27 22:40:45 +0530

    Time: 0 ms (100%), Space: 40.4 MB (80.78%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -3,15 +3,21 @@ class Solution {
         if(sentence.length() < 26) {
             return false;
         }
-        ArrayList<Character>arr = new ArrayList<>(26);
+        // ArrayList<Character>arr = new ArrayList<>(26);
+        // for (char i = 'a'; i <= 'z';i++) {
+        //     arr.add(i);
+        // }
+        // for (int i = 0;i < sentence.length();i++) {
+        //     if(arr.contains(sentence.charAt(i))) {
+        //         arr.remove(Character.valueOf(sentence.charAt(i)));
+        //     }
+        // }
+        // return (arr.isEmpty());
         for (char i = 'a'; i <= 'z';i++) {
-            arr.add(i);
-        }
-        for (int i = 0;i < sentence.length();i++) {
-            if(arr.contains(sentence.charAt(i))) {
-                arr.remove(Character.valueOf(sentence.charAt(i)));
+            if(sentence.indexOf(i) == -1) {
+                return false;
             }
         }
-        return (arr.isEmpty());
+        return true;
         }
 }
