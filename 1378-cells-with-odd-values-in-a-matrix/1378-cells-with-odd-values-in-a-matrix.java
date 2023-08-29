8f56cc9f93be8e7bb0dbde2aeb176a528673ac91
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-08-29 19:36:06 +0530

    Time: 1 ms (91.93%), Space: 40 MB (98.58%) - LeetHub

--- 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.java
+++ 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.java
@@ -30,30 +30,21 @@
 //     }
 // }
 class Solution {
-    public int oddCells(int m, int n, int[][] indices) {
-        int[][] ans = new int[m][n];
-
-        for (int i = 0; i < indices.length; i++) {
-            //row is constant
-            for (int j = 0; j <n; j++) {
-                ans[indices[i][0]][j]++;
-            }
-
-            //column is constant
-            for (int j = 0; j < m; j++) {
-                ans[j][indices[i][1]]++;
-            }
-        }
-        
-        // Counting ODD numbers in the final Matrix
+    public int oddCells(int n, int m, int[][] indices) {
         int count = 0;
-        for (int i= 0; i < m; i++) {
-            for (int j = 0; j <n; j++) {
-                if (ans[i][j] % 2 != 0) {
+        int row[] = new int [n];
+        int col[] = new int [m];
+        for(int x[] : indices)
+        {
+            row[x[0]]++;
+            col[x[1]]++;
+        }    
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+            {
+                if((row[i]+col[j])%2!=0)
                     count++;
-                }
-            }
-        }
+            }        
         return count;
     }
 }
