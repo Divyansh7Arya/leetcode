1860dee6a6ba8b2b9b56f1606f667302f922b459
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-08-29 19:30:03 +0530

    Time: 1 ms (91.93%), Space: 40 MB (98.58%) - LeetHub

--- 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.java
+++ 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.java
@@ -1,31 +1,50 @@
+// class Solution {
+//     public int oddCells(int m, int n, int[][] indices) {
+//         int arr[][] = new int [m][n];
+//         int flag ;
+//         int odd = 0;
+//         for (int[] row : indices) {
+//             flag = 0;
+//             for(int element : row) {
+//                 flag++;
+//                 for(int i = 0;i < m;i++ ) {
+//                     for (int j = 0 ; j < n;j++) {
+//                         if (flag == 1 && i == element){
+//                             arr[i][j]++;
+//                         }
+//                         else if (flag == 2 && j == element) {
+//                             arr[i][j]++;
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//         for(int[] row : arr) {
+//             for (int ele : row ) {
+//                 if (ele % 2 != 0) {
+//                     odd++;
+//                 }
+//             }
+//         }
+//         return odd;
+//     }
+// }
 class Solution {
-    public int oddCells(int m, int n, int[][] indices) {
-        int arr[][] = new int [m][n];
-        int flag ;
-        int odd = 0;
-        for (int[] row : indices) {
-            flag = 0;
-            for(int element : row) {
-                flag++;
-                for(int i = 0;i < m;i++ ) {
-                    for (int j = 0 ; j < n;j++) {
-                        if (flag == 1 && i == element){
-                            arr[i][j]++;
-                        }
-                        else if (flag == 2 && j == element) {
-                            arr[i][j]++;
-                        }
-                    }
-                }
-            }
-        }
-        for(int[] row : arr) {
-            for (int ele : row ) {
-                if (ele % 2 != 0) {
-                    odd++;
-                }
-            }
-        }
-        return odd;
+    public int oddCells(int n, int m, int[][] indices) {
+        int count = 0;
+        int row[] = new int [n];
+        int col[] = new int [m];
+        for(int x[] : indices)
+        {
+            row[x[0]]++;
+            col[x[1]]++;
+        }    
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+            {
+                if((row[i]+col[j])%2!=0)
+                    count++;
+            }        
+        return count;
     }
 }
