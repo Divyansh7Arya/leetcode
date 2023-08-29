4c5b205a637e4dd7880393430243d256dac317f2
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-08-29 19:33:47 +0530

    Time: 2 ms (52.94%), Space: 40.2 MB (87.12%) - LeetHub

--- 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.java
+++ 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.java
@@ -1,4 +1,4 @@
- class Solution {
+// class Solution {
 //     public int oddCells(int m, int n, int[][] indices) {
 //         int arr[][] = new int [m][n];
 //         int flag ;
@@ -29,23 +29,31 @@
 //         return odd;
 //     }
 // }
-public int oddCells(int n, int m, int[][] indices) {
-        int[] rows = new int[n]; //array to hold increments in rows
-        int[] columns = new int[m]; //array to hold increments in columns
-        
-        for(int i = 0; i < indices.length; i++){
-            rows[indices[i][0]] += 1;   //increment the indices of rows array based on "indices" array
-            columns[indices[i][1]] += 1; //increment the indices of columns array based on "indices" array
+class Solution {
+    public int oddCells(int m, int n, int[][] indices) {
+        int[][] ans = new int[m][n];
+
+        for (int i = 0; i < indices.length; i++) {
+            //row is constant
+            for (int j = 0; j <n; j++) {
+                ans[indices[i][0]][j]++;
+            }
+
+            //column is constant
+            for (int j = 0; j < m; j++) {
+                ans[j][indices[i][1]]++;
+            }
         }
         
-        int odds = 0;
-        for(int i = 0; i < rows.length; i++){
-            for(int j = 0; j < columns.length; j++){
-                if((rows[i] + columns[j]) % 2 != 0){ //adding rows[i] and columns[j] will give the value in final array. check whether it is odd or even
-                    odds += 1; 
+        // Counting ODD numbers in the final Matrix
+        int count = 0;
+        for (int i= 0; i < m; i++) {
+            for (int j = 0; j <n; j++) {
+                if (ans[i][j] % 2 != 0) {
+                    count++;
                 }
             }
         }
-        return odds;
+        return count;
     }
- }
+}
