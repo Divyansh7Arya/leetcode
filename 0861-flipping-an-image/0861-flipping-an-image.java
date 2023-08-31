1ef485d984505074845b41d9ebc6b94524f72821
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-08-31 19:55:00 +0530

    Time: 1 ms (48.49%), Space: 43.2 MB (98.16%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.java
+++ 0861-flipping-an-image/0861-flipping-an-image.java
@@ -7,14 +7,15 @@ class Solution {
         int l = image[0].length;
         for(int i = 0;i < l;i++) {
             //Inverting
-             for(int j = 0;j < l;j++) {
-                if(image[i][j] == 0) {
-                    image[i][j] = 1;
-                }
-                else {
-                    image[i][j] = 0;
-                }
-             }
+              for(int j = 0;j < l;j++) {
+                  image[i][j] ^= 1;
+            //     if(image[i][j] == 0) {
+            //         image[i][j] = 1;
+            //     }
+            //     else {
+            //         image[i][j] = 0;
+            //     }
+              }
              // Inverting done
                 start = 0;
                 end = l-1;
