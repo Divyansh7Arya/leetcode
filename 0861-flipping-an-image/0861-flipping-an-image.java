5f668392f452bb01eff8b3f61cd689e086c9c1fa
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-08-31 19:57:33 +0530

    Time: 1 ms (48.49%), Space: 43.8 MB (40.99%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.java
+++ 0861-flipping-an-image/0861-flipping-an-image.java
@@ -6,24 +6,29 @@ class Solution {
         int start,end; // variables to swap
         int l = image[0].length;
         for(int i = 0;i < l;i++) {
-            //Inverting
-              for(int j = 0;j < l;j++) {
-                  image[i][j] ^= 1;
-            //     if(image[i][j] == 0) {
-            //         image[i][j] = 1;
-            //     }
-            //     else {
-            //         image[i][j] = 0;
-            //     }
-              }
+        //     //Inverting
+        //       for(int j = 0;j < l;j++) {
+        //           image[i][j] ^= 1;
+        //     //     if(image[i][j] == 0) {
+        //     //         image[i][j] = 1;
+        //     //     }
+        //     //     else {
+        //     //         image[i][j] = 0;
+        //     //     }
+        //       }
              // Inverting done
                 start = 0;
                 end = l-1;
+            if (l % 2 != 0) {
+                image[i][l / 2] ^= 1;
+            }
                 // swapping
                 while(start < end) {
                 image[i][start] = image[i][start] + image[i][end];
                 image[i][end] = image[i][start] - image[i][end];
                 image[i][start] = image[i][start] - image[i][end];
+                image[i][start] ^= 1;
+                image[i][end] ^= 1;
                 start++;
                 end--;
             }
