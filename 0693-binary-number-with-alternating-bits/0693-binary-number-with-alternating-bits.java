efe06ac0c8bb630aff1e53e6422ff433c348554a
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-09-01 18:37:14 +0530

    Time: 0 ms (100%), Space: 39.4 MB (39.37%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.java
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.java
@@ -4,12 +4,11 @@ class Solution {
         int cnt = 0;
         while (n > 0) {
             int last = n & 1;
-            if (cnt > 0) {
+            if (cnt++ > 0) {
                 if (match == last) {
                     return false;
                 }
             }
-            cnt++;
             match = last;
             n >>= 1;
         }
