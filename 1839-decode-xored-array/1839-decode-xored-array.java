af874b3a8260aea486a8d3183d380134b5dd1532
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-09-01 23:36:15 +0530

    Time: 1 ms (100%), Space: 45.1 MB (84.52%) - LeetHub

--- 1839-decode-xored-array/1839-decode-xored-array.java
+++ 1839-decode-xored-array/1839-decode-xored-array.java
@@ -1,14 +1,24 @@
+// class Solution {
+//     public int[] decode(int[] encoded, int first) {
+//         int [] arr = new int [encoded.length + 1];
+//         arr[0] = first;
+//         //for(int i = 1; i < arr.length; i++) {
+//             //arr[i] = arr[i - 1] ^ encoded[i - 1];
+// // is approach me hame 2 baar peeche {[i-1]} dekhna pad rha hai, this increases time taken
+// // so we do approach jisme sirf ek baar dekhna pade
+//         for(int i = 0; i < encoded.length; i ++) {
+//             arr[i + 1] = arr[i] ^ encoded[i];
+//         }
+//         return arr;
+//     }
+// }
 class Solution {
     public int[] decode(int[] encoded, int first) {
-        int [] arr = new int [encoded.length + 1];
-        arr[0] = first;
-        //for(int i = 1; i < arr.length; i++) {
-            //arr[i] = arr[i - 1] ^ encoded[i - 1];
-// is approach me hame 2 baar peeche {[i-1]} dekhna pad rha hai, this increases time taken
-// so we do approach jisme sirf ek baar dekhna pade
-        for(int i = 0; i < encoded.length; i ++) {
-            arr[i + 1] = arr[i] ^ encoded[i];
+        int[] ans = new int[encoded.length + 1];
+        ans[0] = first;
+        for (int i = 0; i < encoded.length; i++) {
+            ans[i + 1] = ans[i] ^ encoded[i];
         }
-        return arr;
+        return ans;
     }
 }
