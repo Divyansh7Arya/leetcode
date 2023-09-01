0b25d7466f3d69b9bc8d776780e874c1a22a7cb2
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-09-01 23:35:42 +0530

    Time: 2 ms (44.67%), Space: 45.7 MB (5.67%) - LeetHub

--- 1839-decode-xored-array/1839-decode-xored-array.java
+++ 1839-decode-xored-array/1839-decode-xored-array.java
@@ -2,9 +2,12 @@ class Solution {
     public int[] decode(int[] encoded, int first) {
         int [] arr = new int [encoded.length + 1];
         arr[0] = first;
-        for(int i = 1; i < arr.length; i++) {
-            arr[i] = first ^ encoded[i - 1];
-            first = arr[i];
+        //for(int i = 1; i < arr.length; i++) {
+            //arr[i] = arr[i - 1] ^ encoded[i - 1];
+// is approach me hame 2 baar peeche {[i-1]} dekhna pad rha hai, this increases time taken
+// so we do approach jisme sirf ek baar dekhna pade
+        for(int i = 0; i < encoded.length; i ++) {
+            arr[i + 1] = arr[i] ^ encoded[i];
         }
         return arr;
     }
