eaaaceb91e0c36e2d8add0b8fe455b0183b971d3
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-09-08 23:25:55 +0530

    Time: 0 ms (100%), Space: 40.5 MB (91.03%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,14 +1,11 @@
 public class Solution {
-    // you need treat n as an unsigned value
     public int reverseBits(int n) {
-        int binaryNum = 0;
-    
-    for(int i=0;i<32;i++) {
-        binaryNum = binaryNum<<1;                   //multiply by 2
-        binaryNum = binaryNum + (n&1);              //add last digit to binaryNum 
-        n=n>>1;                                     //update n by removing last digit
-    }
-    
-    return binaryNum ;
+        int jagah = 0;
+        for (int i = 0;i < 32;i++) {
+            jagah <<= 1;
+            jagah += n & 1;
+            n >>= 1;
+        }
+        return jagah;
     }
 }
