7b5c5354d318a454d0b59261fda53555b07dd9a9
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-09-09 22:26:12 +0530

    Time: 0 ms (100%), Space: 39.1 MB (83.34%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,6 +1,8 @@
 class Solution {
     public int findComplement(int num) {
         int temp = num, x = 0;
+        // eg number is 1010
+        // we need to make mask 1111
         while(temp != 0) {
             temp >>= 1;
             x <<= 1;
