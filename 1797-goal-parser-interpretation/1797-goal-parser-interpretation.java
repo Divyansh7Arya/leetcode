df6be6e8e78b43d7b41e926996315aef124e9ebb
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-09-12 11:09:54 +0530

    Time: 1 ms (65.77%), Space: 40.6 MB (60.24%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.java
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.java
@@ -1,28 +1,6 @@
-// class Solution {
-//     public String interpret(String command) {
-//         //command = command.replace("()","o");
-//         return command.replace("()","o").replace("(al)","al");
-//     }
-// }
 class Solution {
     public String interpret(String command) {
-        StringBuilder str = new StringBuilder();
-        for(int i =0; i<command.length(); i++){
-         if(command.charAt(i) == 'G'){
-         str.append("G"); 
-         }        
-        else if(command.charAt(i) ==  '(' ){
-        if(command.charAt(i+1) == ')'){
-           str.append("o");
-             i +=1;
-        }  
-            else{
-                str.append("al");
-                i += 3;
-              }
-          }
-            
-        }     
-        return str.toString();
+        //command = command.replace("()","o");
+        return command.replace("()","o").replace("(al)","al");
     }
 }
