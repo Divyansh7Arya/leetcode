a5069bd38d68b34e6a4650b895e6c6b7d876c78d
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-09-12 17:14:47 +0530

    Time: 13 ms (7.55%), Space: 41.3 MB (16.77%) - LeetHub

---
+++ 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        String a = s.substring(0,s.length() / 2);
+        String b = s.substring(s.length() / 2,s.length());
+        System.out.println(a);
+        System.out.println(b);
+        int acnt = 0;
+        int bcnt = 0;
+        for(int i = 0;i < s.length() / 2; i ++) {
+            if(a.charAt(i) == 'a' || a.charAt(i) == 'e' || a.charAt(i) == 'i' || a.charAt(i) == 'o' || a.charAt(i) == 'u' || a.charAt(i) == 'A' || a.charAt(i) == 'E' || a.charAt(i) == 'I' || a.charAt(i) == 'O' || a.charAt(i) == 'U' ) {
+                acnt++;
+            }
+            if(b.charAt(i) == 'a' || b.charAt(i) == 'e' || b.charAt(i) == 'i' || b.charAt(i) == 'o' || b.charAt(i) == 'u' || b.charAt(i) == 'A' || b.charAt(i) == 'E' || b.charAt(i) == 'I' || b.charAt(i) == 'O' || b.charAt(i) == 'U' ) {
+                bcnt++;
+            }
+        }
+        return acnt == bcnt;
+    }
+}
