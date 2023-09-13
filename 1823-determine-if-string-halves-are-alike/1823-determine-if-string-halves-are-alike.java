d22116801b6944d7cc1bd68c997ea32836c7b46d
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-09-13 13:05:07 +0530

    Time: 8 ms (24.69%), Space: 40.9 MB (40.15%) - LeetHub

--- 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.java
@@ -1,46 +1,20 @@
-// class Solution {
-//     public boolean halvesAreAlike(String s) {
-//         StringBuilder a = new StringBuilder(s.substring(0,s.length() / 2));
-//         StringBuilder b = new StringBuilder(s.substring(s.length() / 2,s.length()));
-//         System.out.println(a);
-//         System.out.println(b);
-//         int acnt = 0;
-//         int bcnt = 0;
-//         for(int i = 0;i < s.length() / 2; i ++) {
-//             if(Character.toLowerCase(a.charAt(i)) == 'a' || Character.toLowerCase(a.charAt(i)) == 'e' || Character.toLowerCase(a.charAt(i)) == 'i' || Character.toLowerCase(a.charAt(i)) == 'o' || Character.toLowerCase(a.charAt(i)) == 'u' ) {
-//                 acnt++;
-//             }
-//             if(Character.toLowerCase(b.charAt(i)) == 'a' || Character.toLowerCase(b.charAt(i)) == 'e' || Character.toLowerCase(b.charAt(i)) == 'i' || Character.toLowerCase(b.charAt(i)) == 'o' || Character.toLowerCase(b.charAt(i)) == 'u' ) {
-//                 acnt--;
-//             }
-//         }
-//         return acnt == 0;
-//     }
-// }
 class Solution {
-    public boolean halvesAreAlike(String str) {
-        int n=str.length();
-        int count=0;
-        for(int i=0;i<n/2;i++){
-            if(isVowel(str.charAt(i))){
-                count++;
+    public boolean halvesAreAlike(String s) {
+        String sb = s.toLowerCase();
+        StringBuilder a = new StringBuilder(sb.substring(0,sb.length() / 2));
+        StringBuilder b = new StringBuilder(sb.substring(sb.length() / 2,sb.length()));
+      //  System.out.println(a);
+     //   System.out.println(b);
+        int acnt = 0;
+        int bcnt = 0;
+        for(int i = 0;i < s.length() / 2; i ++) {
+            if((a.charAt(i)) == 'a' || (a.charAt(i)) == 'e' || (a.charAt(i)) == 'i' || (a.charAt(i)) == 'o' || (a.charAt(i)) == 'u' ) {
+                acnt++;
+            }
+            if((b.charAt(i)) == 'a' || (b.charAt(i)) == 'e' || (b.charAt(i)) == 'i' || (b.charAt(i)) == 'o' || (b.charAt(i)) == 'u' ) {
+                acnt--;
             }
         }
-        for(int i=n/2;i<n;i++){
-            if(isVowel(str.charAt(i))){
-                count--;
-            }
-        }
-        if(count==0){
-            return true;
-        }
-        return false;
-    }
-    public boolean isVowel(char ch){
-        if(ch=='a' ||ch=='e' ||ch=='i' ||ch=='o' ||ch=='u'
-            ||ch=='A'||ch=='E' || ch=='I' || ch=='O' || ch=='U'){
-                return true;
-            }
-            return false;
+        return acnt == 0;
     }
 }
