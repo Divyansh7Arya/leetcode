2a1c8090b80a046a3e20e774648990400a9b01f0
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-09-29 19:17:02 +0530

    Time: 1 ms (99.82%), Space: 55.5 MB (59.26%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.java
+++ 0932-monotonic-array/0932-monotonic-array.java
@@ -1,26 +1,26 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        int ele = nums[0];
-        int flag = 0;
-        int i = 0;
-        for( i = 1; i < nums.length;i++) {
-            if(nums[i] != ele) {
-                ele = nums[i];
-                flag++;
-                break;
-            }
-        }
-        if(flag == 0) return true;
-        if(i == nums.length - 1) return true;
-        // stream me get krke 
-        if(ele > nums[0]) {
-            System.out.println(i + "" + ele);
-            for(int j = i + 1; j < nums.length;j++) {
+        // int ele = nums[0];
+        // int flag = 0;
+        // int i = 0;
+        // for( i = 1; i < nums.length;i++) {
+        //     if(nums[i] != ele) {
+        //         ele = nums[i];
+        //         flag++;
+        //         break;
+        //     }
+        // }
+        // if(flag == 0) return true;
+        // if(i == nums.length - 1) return true;
+        // upar vala sab kaam kiya pata lagane ke liye ki arr inc hai ya decreasing 
+        // is se acha compare kro arr 1st element to last
+        if(nums[0] < nums[nums.length - 1]) {
+            for(int j = 1; j < nums.length;j++) {
                 if(nums[j] < nums[j - 1]) return false;
             }
         }
         else {
-            for(int j = i + 1; j < nums.length;j++) {
+            for(int j =  1; j < nums.length;j++) {
                 if(nums[j] > nums[j - 1]) return false;
             }
         }
