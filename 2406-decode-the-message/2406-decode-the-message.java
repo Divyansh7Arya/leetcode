7784201450716a7864ad10d6f9d76c6953bcf760
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-09-30 19:14:39 +0530

    Time: 7 ms (71.41%), Space: 42.9 MB (83.12%) - LeetHub

---
+++ 2406-decode-the-message/2406-decode-the-message.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String decodeMessage(String key, String message) {
+        HashMap<Character,Integer> map = new HashMap();
+        HashMap<Integer,Character> alpha = new HashMap();
+        int cnt = 1;
+        char temp = 'a';
+        for(char x : key.toCharArray()) {
+            if(x != ' ' && map.containsKey(x) == false) {
+                map.put(x,cnt++);
+            }
+        }
+        for(int i = 1; i <= 26;i++ ) alpha.put(i,temp++);
+        StringBuilder s = new StringBuilder();
+        for(char x : message.toCharArray()) {
+            if(x == ' ') s.append(" ");
+            else {
+            int num = map.get(x);
+            s.append(alpha.get(num));
+            }
+        }
+        return s.toString();
+    }
+}
