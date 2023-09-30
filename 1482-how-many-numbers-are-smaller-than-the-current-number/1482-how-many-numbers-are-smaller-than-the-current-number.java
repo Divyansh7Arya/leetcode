97af218c4a52bea91ea68d18b2bbdd81f17e9586
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-09-30 18:29:22 +0530

    Time: 4 ms (87.33%), Space: 43.5 MB (77.34%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,10 +1,23 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int[] a = new int [nums.length];
-        int i = 0;
-        for(int x : nums) {
-            a[i++] = (int)Arrays.stream(nums).filter(w -> w < x).count();
-        }
-        return a;
+        // working
+        // eg 8 1 2 2 3
+        // we will sort it and put it in new arr 
+        // new = 1 2 2 3 8
+        // then we will map the sorted array to map(we will start from last member of arr)
+        // basically we need index of 8 in sorted array to tell how many numbers it is larger than
+        // but there is no indexof in aray
+        // so we store it in hashmap where the value is index of 8
+        HashMap<Integer,Integer> map = new HashMap();
+        int[] arr = new int [nums.length];
+        
+        System.arraycopy(nums, 0, arr,
+                                            0, nums.length);
+
+        
+        Arrays.sort(nums);
+        for(int i = nums.length - 1;i >= 0; i --) map.put(nums[i],i);
+        for(int i = 0; i < nums.length;i++) arr[i] = map.get(arr[i]);
+        return arr;
     }
 }
