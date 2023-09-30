175cf816438a5a8d5fac57fe2c7b41eb9261a665
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-09-30 22:28:59 +0530

    Time: 6 ms (85.45%), Space: 44.1 MB (94.98%) - LeetHub

--- 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.java
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.java
@@ -2,22 +2,40 @@ class Solution {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
         // brute force
         // gs ke upar iterate krenge, jo value hogi usko us size ke list me add krdenge and value ko 0 krdenge taki vo reconsider na ho
+        // List<List<Integer>> ans = new ArrayList();
+        // int l = groupSizes.length;     
+        // for(int i = 0; i < l;i++) {
+        //     if(groupSizes[i] > 0) {
+        //         int size = groupSizes[i];
+        //         List <Integer> temp = new ArrayList();
+        //         // for loop tab tak hi chalega jab tak temp me capacity hogi
+        //         for(int j = 0; j < l && temp.size() < size;j++) {
+        //             if(groupSizes[j] == size) {
+        //                 temp.add(j);
+        //                 groupSizes[j] = 0;
+        //             }
+        //         }
+        //         ans.add(temp);
+        //     }
+        // }
+        // return ans;
+        // optimized map approach
+        HashMap<Integer,List<Integer>> map = new HashMap();
         List<List<Integer>> ans = new ArrayList();
-        int l = groupSizes.length;     
+        int l = groupSizes.length;
         for(int i = 0; i < l;i++) {
-            if(groupSizes[i] != 0) {
-                int size = groupSizes[i];
-                List <Integer> temp = new ArrayList();
-                // for loop tab tak hi chalega jab tak temp me capacity hogi
-                for(int j = 0; j < l && temp.size() < size;j++) {
-                    if(groupSizes[j] == size) {
-                        temp.add(j);
-                        groupSizes[j] = 0;
-                    }
-                }
-                if(temp.size() > 0) ans.add(temp);
+            int size = groupSizes[i];
+            // agar is size ki list map me already hai to usko le lenge vrna nayi bana denge
+            List<Integer> temp = map.getOrDefault(size,new ArrayList());
+            temp.add(i);
+            // updating the map
+            map.put(size,temp);
+            // checking if list gets full
+            if(temp.size() == size) {
+                ans.add(temp);
+                map.remove(size);
             }
+            }
+            return ans;
         }
-        return ans;
     }
-}
