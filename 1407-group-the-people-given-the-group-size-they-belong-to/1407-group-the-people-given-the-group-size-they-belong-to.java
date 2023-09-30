67934098ce2c27dc9e073652e3f281f7ccef2860
Divyansh7Arya <114793895+Divyansh7Arya@users.noreply.github.com>
2023-09-30 21:13:28 +0530

    Time: 8 ms (32.32%), Space: 44.7 MB (15.07%) - LeetHub

---
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        // brute force
+        // gs ke upar iterate krenge, jo value hogi usko us size ke list me add krdenge and value ko 0 krdenge taki vo reconsider na ho
+        List<List<Integer>> ans = new ArrayList();
+        int l = groupSizes.length;     
+        for(int i = 0; i < l;i++) {
+            if(groupSizes[i] != 0) {
+                int size = groupSizes[i];
+                List <Integer> temp = new ArrayList();
+                // for loop tab tak hi chalega jab tak temp me capacity hogi
+                for(int j = 0; j < l && temp.size() < size;j++) {
+                    if(groupSizes[j] == size) {
+                        temp.add(j);
+                        groupSizes[j] = 0;
+                    }
+                }
+                ans.add(temp);
+            }
+        }
+
+
+
+        return ans;
+    }
+}
